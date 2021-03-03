d324261e0610fccc46ef0305f078c9688a260cae
solufa <solufa2020@gmail.com>
2021-03-03 22:24:59 +0900

    chore: clarify files

--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  '**/*.ts': () => 'aspida --build && axios-mock-server --build && tsc --noEmit',
-  '*.{js,ts}': ['eslint --fix --no-ignore', 'git add'],
-  '*.{json,md,yml}': ['prettier --write', 'git add']
-}
